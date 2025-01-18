89cdfca57411396f5c8c5050a37f68c0f4b4166b
slametsampon <slamet@pon.co.id>
2025-01-18 20:43:57 +0700

    Deploy for testing

--- dist/bundle.js
+++ dist/bundle.js
@@ -569,6 +569,7 @@
         <p class="text-center text-gray-600">
           SmartSenseLink is an IoT platform designed for smart applications.
         </p>
+        <p>Testing</p>
       </main>
     `;
     }
